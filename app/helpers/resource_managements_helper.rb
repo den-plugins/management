541b89e17a49f168cefed33b3a54d2840a04f798
lvillasica <lvillasica@lvillasica.(none)>
2011-10-26 18:44:05 +0800

    fix: weekly resource availability.

--- app/helpers/resource_managements_helper.rb
+++ app/helpers/resource_managements_helper.rb
@@ -31,12 +31,12 @@ module ResourceManagementsHelper
     return temp
   end
 
-  def resource_countby(skill, resource_allocations=nil)
+  def resource_countby(skill, week=nil, acctg=nil)
     if @resources_no_limit
-      unless resource_allocations
+      if week.nil?
         @resources_no_limit.select {|r| r.skill == skill and !r.is_resigned}.count
       else
-        @resources_no_limit.select {|r| r.skill == skill and !r.is_resigned and !resource_allocations.zero?}.count
+        @resources_no_limit.select {|r| r.skill == skill and !r.is_resigned and !r.allocations(week, nil, acctg).zero?}.count
       end
     end
   end
