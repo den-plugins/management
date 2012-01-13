e73173ba5dea7f997114deba75ab2f9094302ee8
lvillasica <lvillasica@lvillasica.(none)>
2012-01-13 10:55:49 +0800

    resource management forecast fixed header

--- assets/javascripts/fixed_header.js
+++ assets/javascripts/fixed_header.js
@@ -1,8 +1,13 @@
+/*
+  :in-viewport selector----------------------------------------------------------
+*/
+
 (function(jQuery){jQuery.belowthefold=function(element,settings){var fold=jQuery(window).height()+jQuery(window).scrollTop();return fold<=jQuery(element).offset().top-settings.threshold;};jQuery.abovethetop=function(element,settings){var top=jQuery(window).scrollTop();return top>=jQuery(element).offset().top+jQuery(element).height()-settings.threshold;};jQuery.rightofscreen=function(element,settings){var fold=jQuery(window).width()+jQuery(window).scrollLeft();return fold<=jQuery(element).offset().left-settings.threshold;};jQuery.leftofscreen=function(element,settings){var left=jQuery(window).scrollLeft();return left>=jQuery(element).offset().left+jQuery(element).width()-settings.threshold;};jQuery.inviewport=function(element,settings){return!jQuery.rightofscreen(element,settings)&&!jQuery.leftofscreen(element,settings)&&!jQuery.belowthefold(element,settings)&&!jQuery.abovethetop(element,settings);};jQuery.extend(jQuery.expr[':'],{"below-the-fold":function(a,i,m){return jQuery.belowthefold(a,{threshold:0});},"above-the-top":function(a,i,m){return jQuery.abovethetop(a,{threshold:0});},"left-of-screen":function(a,i,m){return jQuery.leftofscreen(a,{threshold:0});},"right-of-screen":function(a,i,m){return jQuery.rightofscreen(a,{threshold:0});},"in-viewport":function(a,i,m){return jQuery.inviewport(a,{threshold:0});}});})(jQuery);
 
+//------------------------------------------------------------------------------
 
 function scroll_fixed_header(){
-  scrollable = jQuery(".movable_table_container:first");
-  scrollable.scrollLeft(Math.abs(jQuery(".movable_table_container:last #floating_tables_holder").position().left));
+  scrollable = jQuery(".movable_table_container:first #weeks_header_holder");
+  scrollable.css({left: jQuery(".movable_table_container:last #floating_tables_holder").position().left});
 }
 
