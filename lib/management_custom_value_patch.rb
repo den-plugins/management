19b47215575783b3a7f8aed04253b3d2e691d008
Stefany Serino <stefanyserino@gmail.com>
2012-01-31 16:00:44 +0800

    #50120: fix custom_value dates validation

--- lib/management_custom_value_patch.rb
+++ lib/management_custom_value_patch.rb
@@ -7,6 +7,7 @@ module Management
       base.send(:include, InstanceMethods)
       base.class_eval do
         unloadable
+        validate :date_entries_must_be_in_calendar
       end
     end
     
@@ -14,6 +15,17 @@ module Management
     end
     
     module InstanceMethods
+      def date_entries_must_be_in_calendar
+        if custom_field.field_format.eql?('date')
+          begin
+            date = value.to_date
+            errors.add(:value, :not_a_date) if Date.valid_date?(date.year, date.month, date.day).nil?
+          rescue ArgumentError
+            errors.add(:value, :not_a_date)
+          end
+        end
+      end
+      
       def mgt_custom(field)
         custom_field.name.downcase.eql? field.downcase
       end
