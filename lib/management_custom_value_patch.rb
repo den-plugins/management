7e5d87bc3a2e24acc3e9e189ddb041047fcd7d56
Stefany Serino <stefanyserino@gmail.com>
2012-02-02 10:23:48 +0800

    fix: catch nil & blank date values (custom fields)

--- lib/management_custom_value_patch.rb
+++ lib/management_custom_value_patch.rb
@@ -17,7 +17,7 @@ module Management
     module InstanceMethods
       def date_entries_must_be_in_calendar
         if custom_field.field_format.eql?('date')
-          if value
+          if value && !value.blank?
             begin
               date = value.to_date
               errors.add(:value, :not_a_date) if Date.valid_date?(date.year, date.month, date.day).nil?
