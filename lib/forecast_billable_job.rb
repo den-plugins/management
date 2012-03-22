187059da53feb361f5f5371f4ac8cb7abd29ebbd
lvillasica <lvillasica@lvillasica.(none)>
2012-03-22 10:13:34 +0800

    Forecast vs Billable chart - added as of date time

--- lib/forecast_billable_job.rb
+++ lib/forecast_billable_job.rb
@@ -5,7 +5,8 @@ class ForecastBillableJob < Struct.new(:from, :to, :selection, :data, :user_ids)
 
   def perform
     users = User.find(:all, :conditions => "id in (#{user_ids.join(',')})")
-    fb = data.merge({selection.downcase.gsub(' ', '_') => forecast_billable_data(users, (from .. to))}).to_json
+    now = Time.now.strftime('%b %d, %Y %I:%M %p')
+    fb = data.merge({selection.downcase.gsub(' ', '_') => (forecast_billable_data(users, (from .. to)) + [now] )}).to_json
     File.open("#{RAILS_ROOT}/config/forecast_billable.json","w") do |f|
       f.write(fb)
     end
