12f935cfbbf02f8fee78576ef747a244fee3b66d
lvillasica <lvillasica@lvillasica.(none)>
2012-04-02 15:25:59 +0800

    forecast vs billable vs available: minor refactor.

--- app/helpers/resource_managements_helper.rb
+++ app/helpers/resource_managements_helper.rb
@@ -194,7 +194,7 @@ module ResourceManagementsHelper
       tmp_availables, tmp_forecasts, tmp_billables = [], [], []
       ticks << m.first.strftime("%b %Y")
       users.each do |u|
-        tmp_availables << u.total_expected(m.first, m.last, u.projects.map(&:id))
+        tmp_availables << u.total_expected(m.first, m.last, u.memberships.map(&:project_id))
         tmp_forecasts << cost_compute_forecasted_hours(m, u.memberships.all, "billable")
         tmp_billables << u.memberships.all.collect { |mem| mem.spent_time(m.first, m.last, "Billable", true).to_f }.sum
       end
