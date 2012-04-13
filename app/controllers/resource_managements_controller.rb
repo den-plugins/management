8561311a7ea43232981fef5efaafbc0450502d66
Geraldine Milan <blackgemph@yahoo.com>
2012-04-13 16:18:35 +0800

    Refactor updating employee status

--- app/controllers/resource_managements_controller.rb
+++ app/controllers/resource_managements_controller.rb
@@ -180,7 +180,8 @@ class ResourceManagementsController < ApplicationController
         if @user.save
           if !@user.resignation_date.empty? && !@user.resignation_date.nil?
             if @user.resignation_date.to_date < Date.today
-              @user.custom_values.find_by_custom_field_id(23).update_attribute :value, "Resigned"
+              employee_status_field_id = CustomField.find_by_name("Employee Status")
+              @user.custom_values.find_by_custom_field_id(employee_status_field_id).update_attribute :value, "Resigned"
             end
           end
           Mailer.deliver_account_activated(@user) if was_activated
