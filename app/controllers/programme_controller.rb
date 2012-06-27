5ceb2cb41aad635dfaf1a0ea80bf9086f538b629
Normz <johnnormancapule@gmail.com>
2012-06-27 13:24:33 +0800

    #59504(Show only open project in Programme Dashboard)

--- app/controllers/programme_controller.rb
+++ app/controllers/programme_controller.rb
@@ -16,7 +16,7 @@ class ProgrammeController < ApplicationController
     @header = "Programme Dashboard"
     @projects = Project.find(:all, :include => [:manager],
                              :conditions => ["projects.status = ?", Project::STATUS_ACTIVE],
-                             :order => sort_clause)
+                             :order => sort_clause).reject{|x| x.closed?}
     @devt_projects_sorted = @projects.select(&:in_programme?)
     @devt_projects = @devt_projects_sorted.sort_by {|s| s.name.downcase }
 
